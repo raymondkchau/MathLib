be5cea2d547b0614608cfc9a7216bf6824ae652a
Raymond Chau <raymondkietchau@gmail.com>
2017-10-25 15:40:28 -0700

    Fixed loading issues, won't draw image however.

--- MathLib/X-TestSpace/main.cpp
+++ MathLib/X-TestSpace/main.cpp
@@ -5,7 +5,24 @@
 #include "mathutils.h"
 #include "sfwdraw.h"
 
+//initializing public stuff
+unsigned int imgButt = sfw::loadTextureMap("res/avacado.png");
+
+void update()
+{
+	//called per frame
+	sfw::drawTexture(imgButt, 400, 300, 100, 100);
+}
+
 void main()
 {
-	
+	sfw::initContext(800, 600, "Fucking Goddamn Bullshit");
+	sfw::setBackgroundColor(BLACK);
+
+	while (sfw::stepContext())
+	{
+		update();
+	}
+
+	sfw::termContext();
 }
